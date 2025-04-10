32d79ee3b2a6ace35cbcaa89e2c1dec4a3732de2
paulwecker <paulwecker504@gmail.com>
2025-04-10 14:03:01 -0500

    test ioredis railway

--- back/config/plugins.ts
+++ back/config/plugins.ts
@@ -1,36 +1,38 @@
-export default ({ env }) => {
-  // Log the connection string for debugging
-  const redisUrl = env('REDIS_URL', `redis://${env('REDIS_USER', 'default')}:${env('REDIS_PASS', '')}@redis.railway.internal:${env.int('REDIS_PORT', 6379)}/0?family=0`);
-  console.log('Redis Connection URL:', redisUrl);
+export default ({ env }) => ({
+	'redis': {
+		config: {
+			settings: {
+				debug: false,
+				enableRedLock: true,
+				lockDelay: null,
+				lockTTL: 5000,
+				redlockConfig: {
+					driftFactor: 0.01,
+					retryCount: 10,
+					retryDelay: 200,
+					retryJitter: 200
+				}
+			},
+			connections: {
+				default: {
+					connection: {
+						host: env('REDIS_HOST'),
+						port: env('REDIS_PORT', 6379),
+						username: env('REDIS_USER', ''),
+						password: env('REDIS_PASS', ''),
+						db: 0,
+						family: 0
+					},
+					settings: {
+						debug: false
+					}
+				}
+			}
+		}
 
-  return {
-    'redis': {
-      config: {
-        settings: {
-          debug: false,
-          enableRedLock: true,
-          lockDelay: null,
-          lockTTL: 5000,
-          redlockConfig: {
-            driftFactor: 0.01,
-            retryCount: 10,
-            retryDelay: 200,
-            retryJitter: 200
-          }
-        },
-        connections: {
-          default: {
-            connection: redisUrl, // Use hardcoded redis.railway.internal
-            settings: {
-              debug: true
-            }
-          }
-        }
-      }
-    },
-    'weatherbot': {
-      enabled: true,
-      resolve: './src/plugins/weatherbot'
-    }
-  };
-}
+	},
+	'weatherbot': {
+		enabled: true,
+		resolve: './src/plugins/weatherbot'
+	}
+});
